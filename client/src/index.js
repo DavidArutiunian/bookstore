020975b629b2bb229f2e15167e994252c79061b9
Dasha6elka <CB4kot@gmail.com>
2019-11-30 20:52:08 +0300

    Use sessionStorage in redux-localstorage

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from "./containers/app";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
-const store = configureStore();
+const store = configureStore(["auth"]);
 
 ReactDOM.render(
     <Provider store={store}>
--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import createReducer from "./reducers";
+import persistState, { mergePersistedState } from "redux-localstorage";
+import adapter from "redux-localstorage/lib/adapters/sessionStorage";
+import filter from "redux-localstorage-filter";
 
-export default function configureStore(initialState = {}) {
-    const middlewares = [thunk];
-    const enhancers = [applyMiddleware(...middlewares)];
+export default function configureStore(persistPaths = [], initialState = {}) {
+    const reducer = compose(mergePersistedState())(createReducer());
+    const filters = persistPaths.map(key => filter(key));
+    const storage = compose(...filters)(adapter(window.sessionStorage));
+
+    const middleware = [thunk];
 
     const composeEnhancers =
         process.env.NODE_ENV !== "production" &&
@@ -13,16 +19,7 @@ export default function configureStore(initialState = {}) {
             ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
             : compose;
 
-    const store = createStore(createReducer(), initialState, composeEnhancers(...enhancers));
+    const enhancer = composeEnhancers(applyMiddleware(...middleware), persistState(storage));
 
-    store.injectedReducers = {};
-
-    if (module.hot) {
-        module.hot.accept("./reducers", () => {
-            store.replaceReducer(createReducer(store.injectedReducers));
-            store.dispatch({ type: "@@REDUCER_INJECTED" });
-        });
-    }
-
-    return store;
+    return createStore(reducer, initialState, enhancer);
 }
