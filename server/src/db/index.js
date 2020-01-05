f7424feb11d25607230bbd5b5b78193da3576b9c
Dasha6elka <CB4kot@gmail.com>
2020-01-05 14:52:17 +0300

    Create admin of start

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2/promise");
+const argon2 = require("argon2");
 
+let initialized = false;
 let connection = null;
 
 module.exports = async () => {
@@ -10,6 +12,37 @@ module.exports = async () => {
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DB,
         });
+    }
+    const login = process.env.ADMIN_USERNAME || "admin";
+    if (!initialized) {
+        const row = await connection.execute(
+            `
+                SELECT password
+                FROM employee
+                WHERE login = ?
+                ORDER BY id_employee
+                LIMIT 1
+            `,
+            [login],
+        );
+        const admin = row[0][0];
+        if (admin && admin.password) {
+            initialized = true;
+        }
+    }
+    if (!initialized) {
+        const password = process.env.ADMIN_PASSWORD || "password";
+        try {
+            const hash = await argon2.hash(password);
+            await connection.execute(
+                `
+                    INSERT INTO employee(login, password, is_admin)
+                    VALUES(?, ?, ?)
+                `,
+                [login, hash, true],
+            );
+        } catch {}
+        initialized = true;
     }
     return connection;
 };
